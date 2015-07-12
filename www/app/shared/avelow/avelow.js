111226624c1420ae6c835434a173d4d211cda362
Avelow <merceron.alex6@gmail.com>
2015-07-13 01:24:31 +0200

    maj awdFileModel pour upload multiple

--- www/app/shared/avelow/avelow.js
+++ www/app/shared/avelow/avelow.js
@@ -251,12 +251,12 @@
         return {
             restrict: 'A',
             link: function(scope, element, attrs) {
-                var model = $parse(attrs.fileModel);
+                var model = $parse(attrs.awdFileModel);
                 var modelSetter = model.assign;
 
                 element.bind('change', function(){
                     scope.$apply(function(){
-                        modelSetter(scope, element[0].files[0]);
+                        modelSetter(scope, element[0].files);
                     });
                 });
             }
